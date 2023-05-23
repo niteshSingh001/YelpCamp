c99b49afc0e6b0ef6366f34bd145391bf6292c45
Nitesh Singh <niteshsinghm21@gmail.com>
2023-05-23 13:42:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const mongoose = require('mongoose');
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
+const methodOVerride = require('method-override');
+const Campground = require('./models/campground');
+const ExpressError = require("./utils/ExpressError");
+const Review = require('./models/review.js');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
+
+const campgrounds = require('./routes/campground.js');
+const reviews = require('./routes/reviews.js');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "CONNECTION ERROR:"));
+db.once("open", () => {
+    console.log("CONNECTED TO MONGO DATABASE");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOVerride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error')
+    next();
+})
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh  No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("SERVING ON PORT 3000");
+});
+
---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const { places, descriptors } = require('./seedHelpers');
+const cities = require('./cities');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "CONNECTION ERROR:"));
+db.once("open", () => {
+    console.log("CONNECTED TO MONGO DATABASE");
+});
+const sample = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city},${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci reiciendis vitae vero sit. Labore qui enim non numquam, ipsum provident mollitia dolorum, quia iusto repellendus et earum exercitationem officiis ad!",
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
