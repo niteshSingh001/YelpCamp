97772751caa88a752f314459772553db127d78aa
Nitesh Singh <niteshsinghm21@gmail.com>
2023-05-30 21:40:32 +0530

    Added Image Upload feature on Edit Page

--- models/campground.js
+++ models/campground.js
@@ -1,102 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require('./review');
+const Review = require("./review");
 
-const CampgroundSchema = new Schema(
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
+const CampgroundSchema = new Schema({
+  title: String,
+  images: [ImageSchema],
+  price: Number,
+  description: String,
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviews: [
     {
-        title: String,
-        images: [
-            {
-                url:String,
-                filename:String
-            }
-        ],
-        price: Number,
-        description: String,
-        location: String,
-        author: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        reviews: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Review'
-            }
-        ]
-    }
-);
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
 // query middleware
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = mongoose.model("Campground", CampgroundSchema);
--- routes/campground.js
+++ routes/campground.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const catchAsync = require('../utils/catchAsync');
+const express = require("express");
+const catchAsync = require("../utils/catchAsync");
 const router = express.Router();
-const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
-const Campground = require('../models/campground.js');
-const campgrounds = require('../controllers/campgrounds');
-const multer=require('multer');
-const {storage}=require('../cloudinary');
-const upload=multer({storage});
+const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
+const Campground = require("../models/campground.js");
+const campgrounds = require("../controllers/campgrounds");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
-router.route('/')
-    .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn,upload.array('image'),validateCampground,catchAsync(campgrounds.createCampground));
-    
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, campgrounds.deleteCampground);
 
-
-router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, campgrounds.deleteCampground);
-
-
-
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-
-
-
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.renderEditForm)
+);
 
 module.exports = router;
