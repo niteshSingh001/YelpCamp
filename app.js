819c44d96e03b53d324b45d7bdd5e911239ce025
Nitesh Singh <niteshsinghm21@gmail.com>
2023-05-31 23:09:13 +0530

    Added Map

--- app.js
+++ app.js
@@ -1,61 +1,54 @@
-if(process.env.NODE_ENV !== "production")
-{
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-
-
-
-const express = require('express');
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const mongoose = require('mongoose');
-const { campgroundSchema, reviewSchema } = require('./schemas.js')
-const methodOVerride = require('method-override');
-const Campground = require('./models/campground');
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const methodOVerride = require("method-override");
+const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
-const Review = require('./models/review.js');
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user.js')
+const Review = require("./models/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const userRoutes = require('./routes/users.js')
-const campgroundsRoutes = require('./routes/campground.js');
-const reviewsRoutes = require('./routes/reviews.js');
+const userRoutes = require("./routes/users.js");
+const campgroundsRoutes = require("./routes/campground.js");
+const reviewsRoutes = require("./routes/reviews.js");
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "CONNECTION ERROR:"));
 db.once("open", () => {
-    console.log("CONNECTED TO MONGO DATABASE");
+  console.log("CONNECTED TO MONGO DATABASE");
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOVerride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOVerride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
 app.use(session(sessionConfig));
 app.use(flash());
@@ -67,13 +60,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error')
-    next();
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // app.get('/fakeUser', async (req, res) => {
 //     const user = new User({ email: 'ns@gmail.com', username: 'niteshSingh' });
@@ -81,67 +73,34 @@ app.use((req, res, next) => {
 //     res.send(newUser);
 // })
 
-
 const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else {
-        next();
-    }
-}
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
 
-app.use('/', userRoutes)
-app.use('/campgrounds', campgroundsRoutes)
-app.use('/campgrounds/:id/reviews', reviewsRoutes)
-
-
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-
-
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh  No, Something Went Wrong!';
-    res.status(statusCode).render('error', { err });
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh  No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, () => {
-    console.log("SERVING ON PORT 3000");
+  console.log("SERVING ON PORT 3000");
 });
-
--- seeds/index.js
+++ seeds/index.js
@@ -1,48 +1,53 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const { places, descriptors } = require('./seedHelpers');
-const cities = require('./cities');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "CONNECTION ERROR:"));
 db.once("open", () => {
-    console.log("CONNECTED TO MONGO DATABASE");
+  console.log("CONNECTED TO MONGO DATABASE");
 });
 const sample = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
-}
+  return array[Math.floor(Math.random() * array.length)];
+};
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '647092ff0b7e9feaad180599',
-            location: `${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            images:[
-                {
-                  url: 'https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280132/YelCamp/k6c2osglchdoijfxsdoy.jpg',
-                  filename: 'YelCamp/k6c2osglchdoijfxsdoy'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280137/YelCamp/qqwr7ne0jy0k2dnw39wb.jpg',
-                  filename: 'YelCamp/qqwr7ne0jy0k2dnw39wb'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280135/YelCamp/wshxoldf9ymiici2sdrh.jpg',
-                  filename: 'YelCamp/wshxoldf9ymiici2sdrh'
-                }
-              ],
-            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci reiciendis vitae vero sit. Labore qui enim non numquam, ipsum provident mollitia dolorum, quia iusto repellendus et earum exercitationem officiis ad!",
-            price
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "647092ff0b7e9feaad180599",
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202]
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280132/YelCamp/k6c2osglchdoijfxsdoy.jpg",
+          filename: "YelCamp/k6c2osglchdoijfxsdoy",
+        },
+        {
+          url: "https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280137/YelCamp/qqwr7ne0jy0k2dnw39wb.jpg",
+          filename: "YelCamp/qqwr7ne0jy0k2dnw39wb",
+        },
+        {
+          url: "https://res.cloudinary.com/dpgftl7gz/image/upload/v1685280135/YelCamp/wshxoldf9ymiici2sdrh.jpg",
+          filename: "YelCamp/wshxoldf9ymiici2sdrh",
+        },
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci reiciendis vitae vero sit. Labore qui enim non numquam, ipsum provident mollitia dolorum, quia iusto repellendus et earum exercitationem officiis ad!",
+      price,
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
