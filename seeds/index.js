5925034de94cdcc82a48341467f20ee5c8608c8a
Nitesh Singh <niteshsinghm21@gmail.com>
2023-06-01 23:27:28 +0530

    Added Cluster Map on index page

--- seeds/index.js
+++ seeds/index.js
@@ -15,7 +15,7 @@ const sample = (array) => {
 };
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -24,7 +24,9 @@ const seedDB = async () => {
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
         type: "Point",
-        coordinates: [-113.1331, 47.0202]
+        coordinates: [cities[random1000].longitude,
+        cities[random1000].latitude
+        ]
       },
       images: [
         {
