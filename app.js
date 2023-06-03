38d3079e8a0bbdb9180db0a738ca8ef93d77ab73
Nitesh Singh <niteshsinghm21@gmail.com>
2023-06-03 12:30:43 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-const mongoSanitize=require('express-mongo-sanitize');
-const helmet=require('helmet');
 
 
 
@@ -61,54 +59,8 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-app.use(helmet());
 
 
-
-
-const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com",
-  "https://api.tiles.mapbox.com",
-  "https://api.mapbox.com",
-  "https://kit.fontawesome.com",
-  "https://cdnjs.cloudflare.com",
-  "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-  "https://kit-free.fontawesome.com",
-  "https://stackpath.bootstrapcdn.com",
-  "https://api.mapbox.com",
-  "https://api.tiles.mapbox.com",
-  "https://fonts.googleapis.com",
-  "https://use.fontawesome.com",
-];
-const connectSrcUrls = [
-  "https://api.mapbox.com",
-  "https://*.tiles.mapbox.com",
-  "https://events.mapbox.com",
-];
-const fontSrcUrls = [];
-app.use(
-  helmet.contentSecurityPolicy({
-      directives: {
-          defaultSrc: [],
-          connectSrc: ["'self'", ...connectSrcUrls],
-          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-          workerSrc: ["'self'", "blob:"],
-          childSrc: ["blob:"],
-          objectSrc: [],
-          imgSrc: [
-              "'self'",
-              "blob:",
-              "data:",
-              "https://res.cloudinary.com/dpgftl7gz/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-              "https://images.unsplash.com",
-          ],
-          fontSrc: ["'self'", ...fontSrcUrls],
-      },
-  })
-);
 
 app.use(passport.initialize());
 app.use(passport.session());
